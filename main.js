313bcbaea7fca7c737e12a251c0390d99a1f0484
James Duncan <jamesduncan@Jamess-Mac-mini.local>
2024-11-18 15:25:06 +0000

    might be a thing that works?

---
+++ main.js
@@ -0,0 +1,32 @@
+import Flatter from "./flatter.js";
+
+Flatter.attach("test.sqlite")
+
+class User extends Flatter {
+    username = "";
+
+    static SQLiteDefinition = `
+        CREATE TABLE ${this.tablename} (
+            uuid UUID PRIMARY KEY NOT NULL,
+            created DATETIME,
+            "#document" OBJECT
+        );
+    `;
+
+    static {
+        this.init();
+    }
+
+    constructor( anObject = {} ) {
+        super(...arguments);
+        Object.assign(this, anObject)
+    } 
+
+}
+
+
+
+const u1 = new User({ username: 'james' });
+u1.save();
+const u2 = User.load( u1.uuid );
+console.log(u1,u2)
